935d812c68d9acf8640b3abb86d2626e5c9f1d32
asliguler <asligulerr@icloud.com>
2022-06-19 21:21:36 +0300

    .

--- hw3.py
+++ hw3.py
@@ -7,27 +7,95 @@ import sys
 
 def mainFunc(fileName):
     networkDict = {}
+    # visitedDict = {}
     parseTxtNetwork(fileName, networkDict)
+    # for key in networkDict.keys():
+    #     visitedDict[key] = False
+    # processNetwork(networkDict, visitedDict)
     findKCores(networkDict)
 
 
-def findKCores(networkDict : dict):
+# def findKCores(networkDict):
+#     k = 1
+#     flag = True
+#     while k > 0:
+#         numberOfEdges = 0
+#         while flag:
+#             flag = False
+#             for key in list(networkDict.keys()):
+#                 if len(networkDict[key]) < k:
+#                     flag = True
+#                     for item in networkDict[key]:
+#                         networkDict[item].remove(key)
+#                     networkDict.pop(key)
+#         for key in networkDict.keys():
+#             numberOfEdges = numberOfEdges + len(networkDict[key])
+#         if numberOfEdges == 0:
+#             k = -1
+#         else:
+#             print(f"For k = {k} there are {len(networkDict.keys())} proteins.")
+#             k = k + 1
+
+
+# def processNetwork(networkDict, visitedDict):
+#     k = 1
+#     while k > 0:
+#         keys = list(networkDict.keys())
+#         for key in networkDict.keys():
+#             if visitedDict[key] == False:
+#                 dfsUtil(key, visitedDict, networkDict, k, keys)
+#         returnnum = 0
+#         for key in keys:
+#             returnnum = returnnum + len(networkDict[key])
+#         if returnnum <= 0:
+#             k = -1
+#         else:
+#             print(f"for k = {k} - {returnnum}")
+#             print(f"for k = {k} - {len(keys)}")
+#             k = k + 1
+
+
+# def dfsUtil(key, visitedDict, networkDict, k, keys):
+#     visitedDict[key] = True
+#     for item in networkDict[key]:
+#         if len(networkDict[key]) < k:
+#             networkDict[item].remove(key)
+#             if keys.count(key) > 0:
+#                 keys.remove(key)
+#         if visitedDict[item] == False:
+#             if dfsUtil(item, visitedDict, networkDict, k, keys):
+#                 if networkDict[key].count(item) > 0:
+#                     networkDict[key].remove(item)
+#     return len(networkDict[key]) < k
+
+def findKCores(networkDict: dict):
     k = 1
+    degree = {}
+    for key in list(networkDict.keys()):
+        degree[key] = len(networkDict[key])
     while k > 0:
-        numberOfEdges = 0
+        visited = []
+        numOfEdges = 0
         for key in list(networkDict.keys()):
-            if len(networkDict[key]) < k:
-                for item in networkDict[key]:
-                    networkDict[item].remove(key)
-                networkDict.pop(key)
-        for key in networkDict.keys():
-            numberOfEdges = numberOfEdges + len(networkDict[key])
-        if numberOfEdges == 0:
+            if key not in visited:
+                recursiveKCores(key, visited, degree, k, networkDict)
+        for key in list(networkDict.keys()):
+            if degree[key] >= k:
+                numOfEdges = numOfEdges + 1
+        print(f"For k = {k} there are {numOfEdges} proteins.")
+        if numOfEdges == 0:
             k = -1
         else:
-            print(f"For k = {k} there are {numberOfEdges} proteins.")
             k = k + 1
 
+
+def recursiveKCores(key, visited, degree, k, networkDict):
+    visited.append(key)
+    for item in networkDict[key]:
+        if degree[key] < k:
+            degree[item] = degree[item] - 1
+        if item not in visited:
+            recursiveKCores(item, visited, degree, k, networkDict)
 
 
 def parseTxtNetwork(fileName, networkDict):
